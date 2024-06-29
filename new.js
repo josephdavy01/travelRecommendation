9dc483c6162a941692350a5234be85dd32e5d116
josephdavy01 <josephdavy01@gmail.com>
2024-06-29 14:37:32 +0530

    hey

--- new.js
+++
@@ -1,3 +0,0 @@
-const options = { timeZone: 'America/New_York', hour12: true, hour: 'numeric', minute: 'numeric', second: 'numeric' };
-const newYorkTime = new Date().toLocaleTimeString('en-US', options);
-console.log("Current time in New York:", newYorkTime);
